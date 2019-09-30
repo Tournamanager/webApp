32d7288041fdc92f226568d2c864ba5106e3ac45
Geert <gverhagen97@gmail.com>
2019-09-30 14:46:45 +0200

    Firebase gekoppeld

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDoGbEQEA4dFOZJ2Z8KLzcxKGn3OZAM2Jo",
+    authDomain: "tournamanager-6156a.firebaseapp.com",
+    databaseURL: "https://tournamanager-6156a.firebaseio.com",
+    projectId: "tournamanager-6156a",
+    storageBucket: "tournamanager-6156a.appspot.com",
+    messagingSenderId: "869774866440",
+    appId: "1:869774866440:web:537a832d84ed063e5b79bd",
+    measurementId: "G-FSK8V1XTSE"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
