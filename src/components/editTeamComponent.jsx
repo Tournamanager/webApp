276e531c12b577313ba898a577ab527d7220cad9
Michiel Langen <michiel.langen@kpnmail.nl>
2019-11-11 09:57:23 +0100

    added editTeamComponent

---
+++ src/components/editTeamComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class EditTeam extends Component {
+  constructor(props) {
+    state = {};
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default EditTeam;
