fd30d9508035fbd32e0ad9ead0a3bc156ec101f9
Michiel Langen <michiel.langen@kpnmail.nl>
2019-11-18 14:25:44 +0100

    removed temp imports

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import EditTeam from "./components/teamComponents/editTeamComponent";
-
-ReactDOM.render(<EditTeam />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
