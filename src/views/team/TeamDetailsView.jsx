a31e43cc4439ebaf6747bed903741da8bdfead5d
Geert <gverhagen97@gmail.com>
2019-12-02 15:38:05 +0100

    View added

---
+++ src/views/team/TeamDetailsView.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+import { Button, Form, Col, Row, FormControl } from "react-bootstrap";
+import ApiCommunication from "../../services/apicommunication/ApiCommunication";
+
+class TeamDetailsView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+     
+    };
+
+
+  }
+
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+
+}
+
+export default TeamDetailsView;
