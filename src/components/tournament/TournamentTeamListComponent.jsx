aa9e597a8f94ded5316e46610d2437c3746862ec
larsvandriel <lars.van.driel@hotmail.com>
2019-12-09 13:18:41 +0100

    Created component TournamentTeamListComponent

--- src/components/tournament/TournamentTeamListComponent.jsx
+++ src/components/tournament/TournamentTeamListComponent.jsx
@@ -1,8 +1,41 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 class TournamentTeamListComponent extends Component {
+
     render() {
-        return <div>TournamentTeamListComponent</div>
+        return (
+            this.props.teams.length > 1 ?
+                <div>
+                    <div className={"row"}>
+                        <div className={"col-sm-10"}>
+                            <h5>Teams</h5>
+                        </div>
+                        <div className={"col-sm-2"}>
+                            <h6>{this.props.teams.length}/{this.props.numberOfTeams}</h6>
+                        </div>
+                    </div>
+                    <div className={"border"}>
+                        {
+                            this.props.teams.map(team => <div className={"border-bottom  align-self-center"}><p
+                                className={"text-center"}>{team.name}</p></div>)
+                        }
+                    </div>
+                </div> :
+                <div>
+                    <div className={"row"}>
+                        <div className={"col-sm-10"}>
+                            <h5>Teams</h5>
+                        </div>
+                        <div className={"col-sm-2"}>
+                            <h6>{this.props.teams.length}/{this.props.numberOfTeams}</h6>
+                        </div>
+                    </div>
+                    <div className={"border"}>
+                        <p>No Teams have joined the tournament yet!</p>
+                    </div>
+                </div>
+
+        )
     }
 }
 
--- src/views/tournament/TournamentDetailsView.jsx
+++ src/views/tournament/TournamentDetailsView.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import TournamentTeamListComponent from "../../components/tournament/TournamentTeamListComponent";
 import TournamentMatchListComponent from "../../components/tournament/TournamentMatchListComponent";
 import ApiCommunication from "../../services/apicommunication/ApiCommunication";
@@ -8,36 +8,46 @@ class TournamentDetailsView extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            tournament: {
-                name: "TournamentName",
-                teams: [],
-                matches: []
-            }
+            // tournamentId: 1,
+            // tournament: {
+            //     name: "TournamentName",
+            //     teams: [{key:"1", name: "The A Team"}, {key:"2", name: "The B Team"}],
+            //     // teams: [],
+            //     matches: [],
+            //     numberOfTeams: 8,
+            // }
         }
     }
 
     getTournament() {
-        const body = "query{ tournament(id: 1) {name, teams{name}, matches{teamHome {name}, teamAway {name}, date}}}";
-        const vars = "{}";
+        const body = "query q($id:ID!){tournament(id: $id){name, teams{name}, matches{teamHome {name}, teamAway {name}, date}, numberOfTeams}}";
+        const vars = `{ id: ${this.state.tournamentId} }`;
         ApiCommunication.graphQlCallPost(body, vars)
             .then(response => this.setState({tournaments: response.data.data.tournament}))
     }
 
-    componentDidMount()
-    {
+    componentDidMount() {
         this.getTournament()
     }
 
     render() {
-        return (
+        return ( this.state.tournament == null ?
+            <div className={"alert-warning"}>
+                <p> Tournament not found. Try a different tournament! </p>
+            </div> :
             <div>
                 <h1>{this.state.tournament.name}</h1>
-                <h5>Teams</h5>
-                <TournamentTeamListComponent teams={this.state.tournament.teams} />
-                <h5>Matches</h5>
-                <TournamentMatchListComponent matches={this.state.tournament.matches} />
-                <h5>Match History</h5>
-                <TournamentMatchListComponent matches={this.state.tournament.matches} />
+                <div className={"row justify-content-md-center"}>
+                    <div className={"col-sm-4"}>
+                        <TournamentTeamListComponent teams={this.state.tournament.teams} numberOfTeams={this.state.tournament.numberOfTeams} class="col-sm"/>
+                    </div>
+                    <div className={"col-sm-8"}>
+                        <h5>Matches</h5>
+                        <TournamentMatchListComponent matches={this.state.tournament.matches}/>
+                        <h5>Match History</h5>
+                        <TournamentMatchListComponent matches={this.state.tournament.matches}/>
+                    </div>
+                </div>
             </div>
         )
     }
