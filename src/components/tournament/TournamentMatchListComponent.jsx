909dccd2cfb2cc7b161619dff1a5931d5c37f7f3
larsvandriel <lars.van.driel@hotmail.com>
2019-12-09 15:04:49 +0100

    Created TournamentMatchListComponent

--- src/components/tournament/TournamentMatchListComponent.jsx
+++ src/components/tournament/TournamentMatchListComponent.jsx
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
 
 class TournamentMatchListComponent extends Component {
+
+
     render() {
-        return <div>TournamentMatchListComponent</div>
+        return (
+            this.props.matches.length >= 1 ?
+                <div>
+                    <div className={"border"}>
+                        {
+                            this.props.matches.map(match => <div className={"border-bottom  align-self-center"}><p
+                                className={"text-center"}>{match.teamHome.name} VS {match.teamAway.name} {match.date}</p></div>)
+                        }
+                    </div>
+                </div> :
+                <div>
+                    <div className={"border"}>
+                        <p>The tournament is not started yet!</p>
+                    </div>
+                </div>
+        )
     }
 }
 
--- src/views/tournament/TournamentDetailsView.jsx
+++ src/views/tournament/TournamentDetailsView.jsx
@@ -8,14 +8,33 @@ class TournamentDetailsView extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            // tournamentId: 1,
-            // tournament: {
-            //     name: "TournamentName",
-            //     teams: [{key:"1", name: "The A Team"}, {key:"2", name: "The B Team"}],
-            //     // teams: [],
-            //     matches: [],
-            //     numberOfTeams: 8,
-            // }
+            tournamentId: 1,
+            tournament: {
+                name: "TournamentName",
+                teams: [
+                    {
+                        name: "The A Team"
+                    },
+                    {
+                        key: "2", name: "The B Team"
+                    }
+                ],
+                matches: [
+                    {
+                        teamHome: {name: "The A Team"},
+                        teamAway: {name: "The B Team"},
+                        date: "2020-01-01"
+                    },
+                    {
+                        teamHome: {name: "The A Team"},
+                        teamAway: {name: "The B Team"},
+                        date: "2019-12-01"
+                    },
+                ],
+                // teams: [],
+                // matches: [],
+                numberOfTeams: 8,
+            }
         }
     }
 
@@ -30,25 +49,51 @@ class TournamentDetailsView extends Component {
         this.getTournament()
     }
 
+    getUpcomingMatches() {
+        let upcomingMatches = [];
+        for (let match of this.state.tournament.matches) {
+            if (new Date(match.date) >= new Date()) {
+                upcomingMatches.push(match)
+            }
+        }
+        return upcomingMatches
+    }
+
+    getPassedMatches() {
+        let passedMatches = [];
+        for (let match of this.state.tournament.matches) {
+            if (new Date(match.date) < new Date()) {
+                passedMatches.push(match)
+            }
+        }
+        return passedMatches
+    }
+
     render() {
-        return ( this.state.tournament == null ?
-            <div className={"alert-warning"}>
-                <p> Tournament not found. Try a different tournament! </p>
-            </div> :
-            <div>
-                <h1>{this.state.tournament.name}</h1>
-                <div className={"row justify-content-md-center"}>
-                    <div className={"col-sm-4"}>
-                        <TournamentTeamListComponent teams={this.state.tournament.teams} numberOfTeams={this.state.tournament.numberOfTeams} class="col-sm"/>
-                    </div>
-                    <div className={"col-sm-8"}>
-                        <h5>Matches</h5>
-                        <TournamentMatchListComponent matches={this.state.tournament.matches}/>
-                        <h5>Match History</h5>
-                        <TournamentMatchListComponent matches={this.state.tournament.matches}/>
+        return (this.state.tournament == null ?
+                <div className={"alert-warning"}>
+                    <p> Tournament not found. Try a different tournament! </p>
+                </div> :
+                <div>
+                    <h1>{this.state.tournament.name}</h1>
+                    <div className={"row justify-content-md-center"}>
+                        <div className={"col-sm-4"}>
+                            <TournamentTeamListComponent teams={this.state.tournament.teams}
+                                                         numberOfTeams={this.state.tournament.numberOfTeams}
+                                                         class="col-sm"/>
+                        </div>
+                        <div className={"col-sm-8"}>
+                            <div>
+                                <h5>Matches</h5>
+                                <TournamentMatchListComponent matches={this.getUpcomingMatches()}/>
+                            </div>
+                            <div>
+                                <h5>Match History</h5>
+                                <TournamentMatchListComponent matches={this.getPassedMatches()}/>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
         )
     }
 }
