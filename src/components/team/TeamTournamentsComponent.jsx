15c0ce67bb53605ddebd48a5ef835412fa5d5656
Geert <gverhagen97@gmail.com>
2019-12-09 14:30:53 +0100

    CSS fix

--- src/components/team/TeamTournamentsComponent.jsx
+++ src/components/team/TeamTournamentsComponent.jsx
@@ -13,7 +13,7 @@ class TeamTournamentsComponent extends Component {
 
     render() {
         return (
-            <div class="col-md-5" style={{ margin: 'auto' }}>
+            <div class="col-md-5" style={{ margin: '0 auto' }}>
                 <div>
                     <h2 style={{display: 'inline-block'}}>Tournaments</h2>
                     <button style={{verticalAlign: 'super', float: 'right'}} type="button" class="btn btn-dark"><i style={{verticalAlign: 'middle', fontSize: '28px'}} class="material-icons">add_circle</i>
