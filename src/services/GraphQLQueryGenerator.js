76262cedf15954df6d41068cfdd47a46446efc2b
Lloyd van Zaalen <lfb0801@github.com>
2019-12-09 14:54:36 +0100

    added a request generator
    should make sending requests easier

---
+++ src/services/GraphQLQueryGenerator.js
@@ -0,0 +1,40 @@
+class GraphQLRG {
+    static generateBody(_method, _function, _queryData, _variables) {
+        var response = `"query":"${_method}`
+        if (_variables != null) {
+            response += "("
+            _variables.map(variable => {
+                response += `${variable.name}:${variable.type}!,`
+            })
+            response = response.substring(0, response.length - 1) + ")"
+        }
+        response += `{${_function}`
+        if (_variables != null) {
+            response += "("
+            _variables.map(variable => {
+                response += `${variable.name}: ${'$'}${variable.name},`
+            })
+            response = response.substring(0, response.length - 1)
+            response += ")"
+        }
+        if (_queryData != null)
+            response += `{${_queryData}}`
+        response += `}","variables": {`
+        if (_variables != null) {
+            _variables.map(variable => {
+                response += ` "${variable.name}":`
+                if (variable.type == "Int") {
+                    response += `${variable.value},`
+                } else {
+                    if (variable.value == null) {
+                        response += ` "" `
+                    } else
+                        response += `"${variable.value}",`
+                }
+            })
+            response = response.substring(0, response.length - 1)
+        }
+        response += "}"
+        return response
+    }
+}
--- src/services/apicommunication/ApiCommunication.js
+++ src/services/apicommunication/ApiCommunication.js
@@ -23,8 +23,22 @@ class ApiCommunication {
       });
   }
 
+  /**
+   * @author lfb0801
+   * @description a new function to make a graphql request that should be superior to all others
+   * @param {String} _method Choose between query or mutation
+   * @param {String} _function The function from the backend you want to use
+   * @param {String} _queryData The data you want to retrieve from the request, or null if not used
+   * @param {[{name: String, type: any, value: value}]} _variables Type can only be String or Int, or null if not used
+   */
+  static graphQLRequest(_method, _function, _queryData, _variables){
+    return axios.post(
+      this.endpoint,
+      GraphQLRG.generateBody(_method, _queryData, _queryData, _variables),
+      { headers: { "Content-type": "application/json" } }
+    );
+  }
   static graphQlCallPost(body, variables) {
-    console.log(`{"query": "${body}", "variables": ${variables}}`);
     return axios.post(
       this.endpoint,
       `{"query": "${body}", "variables": ${variables}}`,
