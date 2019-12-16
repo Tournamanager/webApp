66f797abcfbb17eae846d23b8a9572fc863815cb
Michiel Langen <michiel.langen@kpnmail.nl>
2019-12-16 11:38:15 +0100

    added createTournament component

---
+++ src/components/tournament/TournamentCreateComponent.jsx
@@ -0,0 +1,99 @@
+import React, { Component } from "react";
+import ApiCommunication from "../../services/apicommunication/ApiCommunication";
+
+class TournamentCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      number: 0,
+      name: "",
+      description: ""
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleNumberChange = this.handleNumberChange.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+  }
+
+  handleNumberChange(event) {
+    this.setState({
+      number: event.target.value
+    });
+  }
+
+  handleNameChange(event) {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  handleDescriptionChange(event) {
+    this.setState({
+      description: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    var tourName = this.state.name;
+    var tourDesc = this.state.description;
+    var tourNum = this.state.number;
+    var user = this.props.userId;
+
+    ApiCommunication.graphQLRequest("mutation", "createTournament", "id", [
+      { tourName, tourDesc, user, tourNum }
+    ]);
+  }
+
+  render() {
+    return (
+      <div style={{ width: "400px", allign: "center" }}>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>Tournament name:</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Tournament name..."
+              onChange={this.handleNameChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Number of teams:</label>
+            <select className="form-control" onChange={this.handleNumberChange}>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+            </select>
+          </div>
+          <div className="from-group">
+            <label>Description</label>
+            <textarea
+              className="form-control"
+              rows="3"
+              onChange={this.handleDescriptionChange}
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <button
+              type="submit"
+              className="btn btn-primary"
+              style={{ marginTop: "20px" }}
+            >
+              Create
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default TournamentCreate;
--- src/views/tournament/AllTournamentsView.jsx
+++ src/views/tournament/AllTournamentsView.jsx
@@ -4,39 +4,39 @@ import ApiCommunication from "../../services/apicommunication/ApiCommunication";
 import SearchList from "../../components/list/SearchList";
 
 class AllTournamentsView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+      tournaments: [],
+      selectedTournament: {}
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            id: "",
-            tournaments:[],
-            selectedTournament:{}
-        };
-    }
+  getAllTournaments() {
+    const body = "query {tournaments{name}}";
+    const vars = "{}";
+    ApiCommunication.graphQlCallPost(body, vars).then(response =>
+      this.setState({ tournaments: response.data.data.tournaments })
+    );
+  }
 
-    getAllTournaments() {
-        const body = "query {tournaments{name}}";
-        const vars = "{}";
-        ApiCommunication.graphQlCallPost(body, vars)
-            .then(response => this.setState({tournaments: response.data.data.tournaments}))
-    }
+  componentDidMount() {
+    this.getAllTournaments();
+  }
 
-    componentDidMount() {
-        this.getAllTournaments();
-    }
+  setActiveTournament(tournament) {
+    alert(tournament.name);
+  }
 
-    setActiveTournament(tournament) {
-        alert(tournament.name)
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>All Active Tournaments</h1>
-                <SearchList objects={this.state.tournaments} src="tournaments"/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>All Active Tournaments</h1>
+        <SearchList objects={this.state.tournaments} src="tournaments" />
+      </div>
+    );
+  }
 }
 
 export default AllTournamentsView;
