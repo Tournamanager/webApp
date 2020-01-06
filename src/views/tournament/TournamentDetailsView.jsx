6fd2705de4005a3c96302002f36d9e05824890a8
lmtjanssen <36262522+lmtjanssen@users.noreply.github.com>
2020-01-06 14:13:24 +0100

    ViewMatch shows match if loaded (#132)

--- src/views/tournament/TournamentDetailsView.jsx
+++ src/views/tournament/TournamentDetailsView.jsx
@@ -6,9 +6,7 @@ import ApiCommunication from "../../services/apicommunication/ApiCommunication";
 class TournamentDetailsView extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-
-    };
+    this.state = {};
 
     this.redirectEdit = this.redirectEdit.bind(this);
     this.startTournament = this.startTournament.bind(this);
@@ -92,6 +90,7 @@ class TournamentDetailsView extends Component {
               <h5>Matches</h5>
               <TournamentMatchListComponent
                 matches={this.getUpcomingMatches()}
+                {...this.props}
               />
             </div>
             <div>
