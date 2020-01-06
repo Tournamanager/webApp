c4cee7d2e68a23da761f39024b93584e1ec626c1
Geert <gverhagen97@gmail.com>
2020-01-06 15:44:59 +0100

    Tournamentlist page styling

--- src/views/tournament/AllTournamentsView.jsx
+++ src/views/tournament/AllTournamentsView.jsx
@@ -20,13 +20,13 @@ class AllTournamentsView extends Component {
     ApiCommunication.graphQLRequest(
       "query",
       "tournaments",
-      "id name numberOfTeams teams {name}"
-    ).then(response =>
+      "id name numberOfTeams description teams {name}"
+    ).then(response => {
       this.setState({
         tournaments: response.data.data.tournaments,
         isSet: true
       })
-    );
+    });
   }
 
   setActiveTournament(tournament) {
@@ -40,11 +40,11 @@ class AllTournamentsView extends Component {
   render() {
     return (
       <div>
-        <h1 className="ml-3">
-            All Active Tournaments
-            <button className="btn btn-dark float-right mr-3 mt-1" onClick={this.redirectToTarget}>
-              <i style={{ verticalAlign: 'middle', fontSize: '28px' }} className="material-icons">add_circle</i>
-            </button>
+        <h1 className="ml-3 text-center">
+          All Active Tournaments
+          <button className="btn btn-dark ml-3 mb-1" onClick={this.redirectToTarget}>
+            <i style={{ verticalAlign: 'middle', fontSize: '28px' }} className="material-icons">add_circle</i>
+          </button>
         </h1>
         <SearchList
           objects={this.state.tournaments}
